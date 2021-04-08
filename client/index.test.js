0e1b0784c6ff46ad107376f6773392b314ecbf58
freweiniS <frena5@yahoo.com>
2021-04-08 10:43:21 +0100

    rename

--- client/index.test.js
+++ client/index.test.js
@@ -1,13 +1,1 @@
-const getData = require('./index')
-const appendChild = jest.fn()
-document.body.appendChild = appendChild
-test('should be defined', () => {
-  expect(getData).toBeDefined()
-})
-
-test('to append to document.body',() => {
-  getData()
-
-  expect(appendChild.mock.calls.length).toBe(1)
-
-})
+const { getData} = require('./index');
